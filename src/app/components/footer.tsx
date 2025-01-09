5b30e15a3ca0349fe7815fb3cade8b491ecfeae5
NailaSolanki24 <rozsolanki2@gmail.com>
2025-01-10 01:14:52 +0500

    Portfolio with Custom css

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,15 +1,15 @@
 // components/Footer.js
-import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'; // Importing social media icons
+import { FaFacebook, FaGithub, FaLinkedin,} from 'react-icons/fa'; // Importing social media icons
 
 export default function Footer() {
   return (
     <footer>
       <div className="footer-content">
         <div className="socials">
-        <a href="https://github.com/NailaSolanki24" target="_blank" rel="noopener noreferrer">      
+        <a href="httpa://github.com/NailaSolanki24" target="_blank" rel="noopener noreferrer">      
             <FaGithub />
           </a>
-          <a href="https://www.linkedin.com/in/naila-solanki-a77425337" target="_blank" rel="noopener noreferrer">
+          <a href="httpa://www.linkedin.com/in/naila-solanki-a77425337" target="_blank" rel="noopener noreferrer">
             <FaLinkedin />
           </a>
           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
@@ -19,5 +19,4 @@ export default function Footer() {
         <p>&copy; 2024 Naila Solanki. All rights reserved.</p>
       </div>
     </footer>
-  );
-}
+  )};
